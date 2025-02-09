32fe11ffe021c03c8da100d4d4229815f60fe8ed
Hritwika12 <hritwika00sarkar@gmail.com>
2025-02-09 20:26:29 +0530

    Add files via upload

---
+++ Assignment 2.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+int a=5, b=20;
+printf("The two nos. and the sum of them are  ");
+printf("%d %d %d", a, b, a+b);
+}
---
+++ Assignment 3.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter two nos. ");
+scanf("%d %d", &a, &b);
+c=b-a;
+printf("The two nos. and the substraction of them are ");
+printf("%d %d %d", a, b, c);
+}
